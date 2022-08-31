dc93169b9fd717043d78e3f17d5ee990df36fa6f
DblNbKA <104922491+DblNbKA@users.noreply.github.com>
2022-08-31 12:51:40 +0300

    Update pin.py

--- pin.py
+++ pin.py
@@ -5,6 +5,6 @@ bot = telebot.TeleBot('Your telegram bot token') #Instead of "Your telegram bot 
 @bot.message_handler(commands=['start']) #The bot will respond to the /start command
 
 def pin(message): #function
-    bot.send_message(message.chat.id, 'Your text with a greeting, explanation, or something similar, depending on what you need') #sending message by bot
-    pinn = bot.send_message(message.chat.id, 'The message you want to pin').message_id #sending the message you want to pin and saving its ID with .message_id
+    bot.send_message(message.chat.id, 'Your text with a greeting, explanation, or something similar, depending on what you need') #Sending message by bot
+    pinn = bot.send_message(message.chat.id, 'The message you want to pin').message_id #Sending the message you want to pin and saving its ID with .message_id
     bot.pin_chat_message(message.chat.id, message_id = pinn) #Pinning a message
